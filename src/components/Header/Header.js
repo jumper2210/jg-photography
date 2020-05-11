e4629d49b390312261e5579e60e5b0eda36075db
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2020-05-11 12:40:03 +0200

    basic setup - added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+const Header = () => {
+  return (
+    <section className="header">
+      <h1>Siema</h1>
+    </section>
+  );
+};
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+
+const Home = (props) => {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+};
+export default Home;
