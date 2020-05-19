b9c22f965d5996de7cbe14e821b50df6faefa528
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2020-05-19 11:09:23 +0200

    parallax effect: added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./Banner.scss";
 import "../../sass/_typography.scss";
-
-const Banner = React.memo((props) => {
-  return <section className="banner" id="banner"></section>;
-});
+import { Parallax } from "react-parallax";
+import picture from "../../assets/images/banner_proposition1.jpg";
+const Banner = (props) => {
+  return (
+    <section className="banner" id="banner">
+      <Parallax
+        className="banner__pic"
+        bgImage={picture}
+        strength={600}
+      ></Parallax>
+    </section>
+  );
+};
 
 export default Banner;
