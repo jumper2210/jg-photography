1ca14f78178d13ba247703a9e58f32e317da315f
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2022-07-09 17:19:21 +0200

    Changed site key

--- src/env.js
+++ src/env.js
@@ -1,2 +1,2 @@
-export const SITE_RECAPTCHA_KEY = "6LeKm-wZAAAAAOIBWv8TNIrRUmE_3eiYlPqpg1QI"
+export const SITE_RECAPTCHA_KEY = "6Lfcj9kgAAAAAAjRA1M7gU-angx0jh9Hc5eTeCPa"
 export const GET_FORM_KEY = "1bbcb540-7206-4166-a7fc-f14673b2f41b"
